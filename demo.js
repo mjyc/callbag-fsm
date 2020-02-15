671fd72482710bc71c154a85b5eb212bd3eb8079
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2020-02-14 22:52:56 -0800

    Update

--- demo.js
+++ demo.js
@@ -1,6 +1,8 @@
-const { forEach, map, pipe, scan, share } = require("callbag-basics");
+const { forEach, fromIter, map, pipe, scan, share } = require("callbag-basics");
 const dropAfter = require("callbag-drop-after");
+const dropRepeats = require("callbag-drop-repeats");
 const random = require("random");
+const zip = require("lodash.zip");
 const { run, subscribe, hrun } = require("./index");
 
 const makeSimHuman = ({ dropAfterPredicate = () => true } = {}) => {
@@ -73,8 +75,6 @@ const makeSimHuman = ({ dropAfterPredicate = () => true } = {}) => {
     }
   );
 
-  // TODOs
-  // 7. switch the example into a simpler example and provide a mermaid diagram
   const states = pipe(
     runSim,
     scan(
@@ -99,7 +99,45 @@ const makeSimHuman = ({ dropAfterPredicate = () => true } = {}) => {
   };
 };
 
-const simHuman = makeSimHuman({ dropAfterPredicate: x => x.i === 15 });
+// used for generating sensor inputs
+const resample = (trace, interval) => {
+  const arr = [];
+  pipe(
+    fromIter(trace),
+    map(x =>
+      Object.assign({}, x, {
+        stamp: Math.floor(x.stamp / interval) * interval
+      })
+    ),
+    dropRepeats((a, b) => a.stamp === b.stamp), // downsampling
+    forEach(x => arr.push(x))
+  );
+
+  function* range(from, to) {
+    let i = from;
+    while (i <= to) {
+      yield i;
+      i++;
+    }
+  }
+
+  const out = [];
+  let arri = 0;
+  pipe(
+    fromIter(
+      range(arr[0].stamp / interval, arr[arr.length - 1].stamp / interval)
+    ),
+    forEach(i => {
+      // upsampling
+      const newarri = arr.findIndex(x => x.stamp === i * interval);
+      arri = newarri === -1 ? arri : newarri;
+      out.push(Object.assign({}, arr[arri], { stamp: i * interval }));
+    })
+  );
+  return out;
+};
+
+const simHuman = makeSimHuman({ dropAfterPredicate: x => x.i === 16 });
 
 const done = {};
 const simHumanRecorded = Object.keys(simHuman).reduce((prev, k) => {
@@ -110,8 +148,62 @@ const simHumanRecorded = Object.keys(simHuman).reduce((prev, k) => {
       complete: () => {
         done[k] = true;
         if (Object.keys(done).length === Object.keys(simHuman).length) {
-          // done
-          console.log(JSON.stringify({ input: simHumanRecorded }, null, 2));
+          // done generating FSM traces and now to generating sensor inputs
+          const samplingInterval = 100; // 1hz
+          const generateNoseAngle = {
+            intention_center: {
+              facing_center: random.uniform(-20, 20),
+              facing_away: () =>
+                random.boolean()
+                  ? random.uniform(-60, -20)()
+                  : random.uniform(20, 60)()
+            },
+            intention_away: {
+              facing_center: random.uniform(-20, 20),
+              facing_away: () =>
+                random.boolean()
+                  ? random.uniform(-60, -20)()
+                  : random.uniform(20, 60)()
+            }
+          };
+          // preprocessing to
+          // 1. remove the duplicates at transition points; keep 2nd ones
+          // 2. remove the very last switched data point
+          Object.keys(simHumanRecorded).map(k => {
+            simHumanRecorded[k] = simHumanRecorded[k]
+              .slice(0)
+              .reverse()
+              .filter(
+                (x, i, arr) => arr.findIndex(y => y.stamp === x.stamp) === i
+              )
+              .reverse()
+              .slice(0, -1);
+          });
+          const simHumanRecordedResampled = Object.keys(
+            simHumanRecorded
+          ).reduce((prev, k) => {
+            prev[k] = resample(simHumanRecorded[k], samplingInterval);
+            return prev;
+          }, {});
+          const noseAngle = zip(
+            simHumanRecordedResampled.intention.map(x => x.value),
+            simHumanRecordedResampled.action.map(x => x.value),
+            simHumanRecordedResampled.action.map(x => x.stamp)
+          ).map(([intention, action, stamp]) => ({
+            value: generateNoseAngle[intention][action](),
+            stamp
+          }));
+
+          // print results
+          console.log(
+            JSON.stringify(
+              {
+                input: Object.assign({}, simHumanRecorded, { noseAngle })
+              },
+              null,
+              2
+            )
+          );
         }
       }
     })(simHuman[k]);
